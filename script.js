13eafb293f241cc3bec8d5e84dcf0604ece8bb72
Zovma <dim.borzov@gmail.com>
2022-03-03 18:24:47 +0300

    page-proofs finish

--- script.js
+++ script.js
@@ -38,150 +38,275 @@ function createElements() {
 
     switch (progress) {
         case 0:
-            welcom();
-            progress++;
+            scriptConstructor();
             break;
         case 1:
+            welcom();
+            break;
+        case 2:
             missionCall();
-            progress++;
             break;
         case 3:
-            console.log("Доход равен 300");
+            definitionNeed();
+            break;
+        case 4:
+            selectionSaleProdukt();
+            break;
+        case 5:
+            useMiting();
+            break;
+        case 6:
+            useMitingPickUp();
+            break;
+        case 7:
+            useMitingPickUpOnline();
+            break;
+        case 8:
+            useMitingPickUpOnlineOurOffice();
+            break;
+        case 9:
+            useMitingPickUpOnlineСlientOffice();
+            break;
+        case 10:
+            useMitingPickUpOflainNoMatterWhere();
+            break;
+        case 11:
+            useMitingProductPresentation();
+            break;
+        case 12:
+            useMitingProductPresentationOffline();
+            break;
+        case 13:
+            useMitingProductPresentationOfflineOurOffice();
+            break;
+        case 14:
+            useMitingProductPresentationOfflineClientOffice();
+            break;
+        case 15:
+            useMitingProductPresentationOfflineNoMatterWhere();
+            break;
+        case 16:
+            useMitingProductPresentationOnline();
+            break;
+        case 17:
+            useMitingProductPresentationNoMatterWhere();
+            break;
+        case 18:
+            sendingCommercialOffer();
+            break;
+        case 19:
+            finish();
             break;
     }
 }
 
 
+function scriptConstructor() {
+    createHeader("Конструктор скрипта")
+    createDivPInput('Электронная почта')
+    progress++;
+}
+
+
 
 function welcom() {
+    createHeader("Приветствие")
+    createDivPInput("Напишите должность сотрудника, который звонит");
+    createDivPSpanInput("Напишите форму вашей организации", 'Например: "компания", "юридическая фирма", "автосервис", "салон красоты"');
+    createDivPInput("Напишите название вашей организации");
+    progress++
+}
+
+function missionCall() {
+    createHeader("Цель звонка")
+    createDivPSpanInput("Напишите коротко о деятельности вашей организации и о тех реальных преимуществах, которые вы имеете в сравнении с конкурентами", 'Например: Мы занимаемся предоставлением пропусков для грузовых автомобилей в Москву. Взаимодействуя напрямую с департаментом транспорта имеем очень быстрое оформление и высокую степень одобрения - 99%')
+    createDivPRadio('Напишите цель данного звонка', [' Выявление потребностей', ' Подбор и продажа продукта', ' Назначение встречи', ' Отправка коммерческого предложения']);
+    progress++
+}
+
+function definitionNeed() {
+    createHeader("Выявление потребностей")
+    createDivPSpanInput("Напишите вопросы, выявляющие потребность, которые нужно задать клиенту, в той последовательности, в которой вы их будете задавать", "Например: Какой транспорт хотите застраховать? На какой срок обычно страхуете транспорт? Какие проблемы обычно возникают при страховании и дальнейшем использовании?")
+    createDivPSpanInput("Напишите в течение какого количества дней ваше предложение будет готово для клиента", "Например: 1 день")
+    createDivPSpanInput("Напишите сколько времени вам с клиентом понадобится на обсуждение предложения на следующем звонке", "Например: 15 минут")
+    progress++
+}
+
+function selectionSaleProdukt() {
+    createHeader("Подбор и продажа продукта");
+    createDivPSpanInput("Напишите вопросы, выявляющие потребность, которые нужно задать клиенту, в той последовательности, в которой вы их будете задавать", "Например: Какой транспорт хотите застраховать? На какой срок обычно страхуете транспорт? Какие проблемы обычно возникают при страховании и дальнейшем использовании?")
+    createDivPSpanInput("Напишите последовательно все этапы оформления продукта после положительного решения клиента", 'Например: Сейчас я отправлю вам ссылку в Телеграм. Вам необходимо будет пройти по ней и нажать "Купить". После чего, ввести свои данные и данные вашей карты. Далее нажать кнопку "Оплатить"')
+    progress++
+}
+
+function useMiting() {
+    createHeader("Назначение встречи");
+    createDivPRadio('Напишите цель встречи', [' Знакомство', ' Презентация продукта']);
+    progress++
+}
+
+function useMitingPickUp() {
+    createHeader("Назначение встречи. Знакомство");
+    createDivPRadio('Напишите формат встречи, который предполагается в вашей компании', [' Offline', ' Online', ' Формат встречи не имеет значения']);
+    progress++
+}
+
+function useMitingPickUpOnline() {
+    createHeader("Назначение встречи. Знакомство. Offline");
+    createDivPRadio('Напишите, какой вариант встречи возможен', [' Наш офис', ' Офис клиента', ' Не важно где пройдет встреча']);
+    progress++
+}
+
+function useMitingPickUpOnlineOurOffice() {
+    createHeader("Назначение встречи. Знакомство. Offline. Наш офис");
+    createDivPSpanInput("Напишите адрес вашего офиса ", "Например: Невский проспект, дом 35, БЦ Атриум");
+    createDivPSpanInput("Напишите, сколько времени потребуется на встречу", "Например: 1 час");
+    progress++
+}
+
+function useMitingPickUpOnlineСlientOffice() {
+    createHeader("Назначение встречи. Знакомство. Offline. Офис клиента");
+    createDivPSpanInput("Напишите, сколько времени потребуется на встречу", "Например: 1 час");
+    progress++
+}
+
+function useMitingPickUpOflainNoMatterWhere() {
+    createHeader("Назначение встречи. Знакомство. Offline. Не важно где пройдет встреча");
+    createDivPSpanInput("Напишите, сколько времени потребуется на встречу", "Например: 1 час");
+    progress++
+}
+
+function useMitingProductPresentation() {
+    createHeader("Назначение встречи. Презентация продукта");
+    createDivPRadio('Напишите формат встречи, который предполагается в вашей компании', [' Offline', ' Online', ' Формат встречи не имеет значения']);
+    progress++
+}
+
+function useMitingProductPresentationOffline() {
+    createHeader('Назначение встречи. Презентация продукта. Offline');
+    createDivPRadio('Напишите, какой вариант встречи возможен', [' Наш офис', ' Офис клиента', ' Не важно где пройдет встреча'])
+    progress++
+}
+
+function useMitingProductPresentationOfflineOurOffice() {
+    createHeader('Назначение встречи. Презентация продукта. Offline. Наш офис');
+    createDivPSpanInput('Напишите вопросы, выявляющие потребность, которые нужно задать клиенту, в той последовательности, в которой вы их будете задавать', 'Например: Какой транспорт хотите застраховать? На какой срок обычно страхуете транспорт? Какие проблемы обычно возникают при страховании и дальнейшем использовании?')
+    createDivPSpanInput('Напишите адрес вашего офиса', 'Например: Невский проспект, дом 35, БЦ Атриум');
+    createDivPSpanInput('Напишите, сколько времени потребуется на встречу', 'Например: 1 час');
+    progress++
+}
+
+function useMitingProductPresentationOfflineClientOffice() {
+    createHeader('Назначение встречи. Презентация продукта. Offline. Офис клиента');
+    createDivPInput('Напишите вопросы, выявляющие потребность, которые нужно задать клиенту, в той последовательности, в которой вы их будете задавать', 'Например: Какой транспорт хотите застраховать? На какой срок обычно страхуете транспорт? Какие проблемы обычно возникают при страховании и дальнейшем использовании?');
+    createDivPInput('Напишите, сколько времени потребуется на встречу', 'Например: 1 час')
+    progress++
+}
+
+function useMitingProductPresentationOfflineNoMatterWhere() {
+    createHeader('Назначение встречи. Презентация продукта. Offline. Не важно где пройдет встреча');
+    createDivPSpanInput('Напишите вопросы, выявляющие потребность, которые нужно задать клиенту, в той последовательности, в которой вы их будете задавать', 'Например: Какой транспорт хотите застраховать? На какой срок обычно страхуете транспорт? Какие проблемы обычно возникают при страховании и дальнейшем использовании?')
+    createDivPSpanInput('Напишите, сколько времени потребуется на встречу', 'Например: 1 час')
+    progress++
+}
+
+function useMitingProductPresentationOnline() {
+    createHeader('Назначение встречи. Презентация продукта. Online');
+    createDivPSpanInput('Напишите вопросы, выявляющие потребность, которые нужно задать клиенту, в той последовательности, в которой вы их будете задавать', 'Например: Какой транспорт хотите застраховать? На какой срок обычно страхуете транспорт? Какие проблемы обычно возникают при страховании и дальнейшем использовании?');
+    createDivPInput('Напишите какое приложение вы будете использовать для связи с клиентом', 'Например: Zoom, Google Meet, Skype');
+    createDivPInput('Напишите, сколько времени потребуется на встречу', 'Например: 1 час');
+    progress++
+}
+
+function useMitingProductPresentationNoMatterWhere() {
+    createHeader('Назначение встречи. Презентация продукта. Формат встречи не имеет значения');
+    createDivPSpanInput('Напишите вопросы, выявляющие потребность, которые нужно задать клиенту, в той последовательности, в которой вы их будете задавать', 'Например: Какой транспорт хотите застраховать? На какой срок обычно страхуете транспорт? Какие проблемы обычно возникают при страховании и дальнейшем использовании?')
+    createDivPInput('Напишите, сколько времени потребуется на встречу', 'Например: 1 час')
+    progress++
+}
+
+function sendingCommercialOffer() {
+    createHeader('Отправка коммерческого предложения')
+    createDivPSpanInput('Напишите вопросы, выявляющие потребность, которые нужно задать клиенту, в той последовательности, в которой вы их будете задавать', 'Например: Какой транспорт хотите застраховать? На какой срок обычно страхуете транспорт? Какие проблемы обычно возникают при страховании и дальнейшем использовании?')
+    progress++
+}
+
+function finish() {
+    createHeader('Завершение')
+    createDivPSpanInput('На этом закончим. Напишите, как вы вообще?', 'Например: Отлично, продуктивно, интересно');
+    progress++
+}
+
+
+
+
+
+
+
+
+
+
+
+
+function createHeader(contentH1) {
+    let h1 = document.createElement("h1");
+    let texth1 = document.createTextNode(contentH1);
+    h1.appendChild(texth1);
+    container.appendChild(h1);
+}
+
+function createDivPSpanInput(contentP, contentSpan) {
     let div = document.createElement("div");
-    div.className = "change"
     let p = document.createElement("p");
     let input = document.createElement("input");
-    let elemText = document.createTextNode("Напишите должность сотрудника, который звонит");
+    let span = document.createElement("span");
+    let elemText = document.createTextNode(contentP);
+    let elemText1 = document.createTextNode(contentSpan);
+    p.appendChild(elemText);
+    span.appendChild(elemText1)
+    div.appendChild(p);
+    div.appendChild(span);
+    div.appendChild(input);
+    container.appendChild(div);
+}
+
+function createDivPInput(contentP) {
+    let div = document.createElement("div");
+    let p = document.createElement("p");
+    let input = document.createElement("input");
+    let elemText = document.createTextNode(contentP);
     p.appendChild(elemText);
     div.appendChild(p);
     div.appendChild(input);
     container.appendChild(div);
-
-    let div2 = document.createElement("div");
-    div2.className = "change"
-    let p2 = document.createElement("p");
-    let input2 = document.createElement("input");
-    let span = document.createElement("span");
-    let elemText2 = document.createTextNode("Напишите форму вашей организации");
-    let elemText22 = document.createTextNode('Например: "компания", "юридическая фирма", "автосервис", "салон красоты"')
-    p2.appendChild(elemText2);
-    span.appendChild(elemText22)
-    div2.appendChild(p2);
-    div2.appendChild(span);
-    div2.appendChild(input2);
-    container.appendChild(div2);
-
-    let div3 = document.createElement("div");
-    div3.className = "change"
-    let p3 = document.createElement("p");
-    let input3 = document.createElement("input");
-    let elemText3 = document.createTextNode("Напишите название вашей организации");
-    p3.appendChild(elemText3);
-    div3.appendChild(p3);
-    div3.appendChild(input3);
-    container.appendChild(div3);
-
 }
 
-function missionCall() {
+function createDivPRadio(contentP, contentRadio) {
+    let div = document.createElement("div");
+    let p = document.createElement("p");
+    let elemText = document.createTextNode(contentP);
+    p.appendChild(elemText);
+    div.appendChild(p)
 
-    let h2 = document.createElement("h2");
-    let textH2 = document.createTextNode("Цель звонка");
-    h2.appendChild(textH2)
-    console.log(container);
-    container.appendChild(h2);
+    for (let i = 0; i < contentRadio.length; i++) {
+        div.appendChild(createRadio(contentRadio[i]));
+    }
 
-    let div1 = document.createElement("div");
-    div1.className = "change"
-    let p1 = document.createElement("p");
-    let input1 = document.createElement("input");
-    let span1 = document.createElement("span");
-    let elemText1 = document.createTextNode("Напишите коротко о деятельности вашей организации и о тех реальных преимуществах, которые вы имеете в сравнении с конкурентами");
-    let elemText11 = document.createTextNode('Например: Мы занимаемся предоставлением пропусков для грузовых автомобилей в Москву. Взаимодействуя напрямую с департаментом транспорта имеем очень быстрое оформление и высокую степень одобрения - 99%');
-    p1.appendChild(elemText1);
-    span1.appendChild(elemText11)
-    div1.appendChild(p1);
-    div1.appendChild(span1);
-    div1.appendChild(input1);
-    container.appendChild(div1);
+    function createRadio(content) {
+        let radio = document.createElement("input");
+        radio.type = "radio";
+        radio.value = content;
+        radio.name = "radio";
+        let span = document.createElement("span");
+        let textSpan = document.createTextNode(content);
+        span.appendChild(textSpan);
 
-
-
-
-    let div2 = document.createElement("div");
-    
-    let p2 = document.createElement("p");
-    let textP2 = document.createTextNode("Выберете цель данного звонка");
-    p2.appendChild(textP2)
-
-    let radio1 = document.createElement("input");
-    radio1.type = "radio";
-    radio1.value = "Выявление потребностей";
-    radio1.name = "mission";
-    let span2 = document.createElement("span");
-    let textSpan2 = document.createTextNode("  Выявление потребностей (перед звонком с презентацией продукта)");
-    span2.appendChild(textSpan2);
-    
-    let radio2 = document.createElement("input");
-    radio2.type = "radio";
-    radio2.value = "Подбор и продажа продукта";
-    radio2.name = "mission";
-    let span3 = document.createElement("span");
-    let textSpan3 = document.createTextNode("  Подбор и продажа продукта (здесь и сейчас)");
-    span3.appendChild(textSpan3);
-
-    let radio3 = document.createElement("input");
-    radio3.type = "radio";
-    radio3.value = "Назначение встречи";
-    radio3.name = "mission";
-    let span4 = document.createElement("span");
-    let textSpan4 = document.createTextNode("  Назначение встречи (знакомство или презентация продукта)");
-    span4.appendChild(textSpan4);
-
-    let radio4 = document.createElement("input");
-    radio4.type = "radio";
-    radio4.value = "Отправка коммерческого предложения";
-    radio4.name = "mission";
-    let span5 = document.createElement("span");
-    let textSpan5 = document.createTextNode("  Отправка коммерческого предложения (здесь и сейчас)");
-    span5.appendChild(textSpan5);
-
-    div2.appendChild(p2);
-
-
-    let pRadio1 = document.createElement("p");
-    pRadio1.appendChild(radio1)
-    pRadio1.appendChild(span2)
-    div2.appendChild(pRadio1);
-
-
-    let pRadio2 = document.createElement("p");
-    pRadio2.appendChild(radio2)
-    pRadio2.appendChild(span3)
-    div2.appendChild(pRadio2);
-
-    let pRadio3 = document.createElement("p");
-    pRadio3.appendChild(radio3)
-    pRadio3.appendChild(span4)
-    div2.appendChild(pRadio3);
-
-    let pRadio4 = document.createElement("p");
-    pRadio4.appendChild(radio4)
-    pRadio4.appendChild(span5)
-    div2.appendChild(pRadio4);
-
-    container.appendChild(div2);
-
-
-
-
+        let pRadio = document.createElement("p");
+        pRadio.appendChild(radio)
+        pRadio.appendChild(span)
+        return pRadio;
+    }
+    container.appendChild(div);
 
 }
 
